0ef3b51a659074ad2b1d8f60d5bbe787f413683b
loorens <dominik.tom@gmail.com>
2015-11-02 10:59:19 +0100

    zakupyyyy

--- controller.js
+++ controller.js
@@ -3,3 +3,16 @@ var testApp = angular.module('testApp',[]);
 testApp.controller('HelloController', function ($scope){
 	$scope.greeting = {text: 'Hello³'};
 });
+
+
+testApp.controller('CartController', function ($scope){
+	$scope.items = [
+		{title: "wódka", qnt: 5, price: 25},
+		{title: "szmpan", qnt: 1, price: 100},
+		{title: "piwo", qnt: 15, price: 5}
+	];
+	
+	$scope.remove = function(index){
+		$scope.items.splice(index,1);
+	}
+});
